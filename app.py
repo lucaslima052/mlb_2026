20715643459c2bbcb2e857502f5b53f3815576af
lucaslima052 <lucaslima052@gmail.com>
2026-09-14 04:46:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def get_data_dict():
     tiebreakers_3way = []
     try:
         bj_name = "Toronto Blue Jays"
-        target_teams = [t for t, cat in team_categories.items() if cat in ['critical', 'important', 'relevant'] and t != bj_name]
+        target_teams = [t for t, cat in team_categories.items() if cat in ['critical', 'important'] and t != bj_name]
         
         # --- 2-Way Tiebreakers ---
         for team in target_teams:
